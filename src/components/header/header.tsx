e757364ad8b02c499e10fe9881ca6c8760083e44
JerryRivera609 <jerry609rivera@gmail.com>
2025-01-14 22:44:57 -0500

    Corrección de nombre

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,7 +5,9 @@ function header(){
         <>
             <header>
                 <div className="main-Header">
-                    
+                    <div className="nombre-Header">
+                        <h2>Jerry Rivera</h2>
+                    </div>
                     <div className="links-Header">
                         <ul>
                             <li><a href="#">Home</a></li>
