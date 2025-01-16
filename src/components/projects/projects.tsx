bd030b15e607a6e9e74b5dc66ba9cb0b0c48efa0
JerryRivera609 <jerry609rivera@gmail.com>
2025-01-15 22:53:52 -0500

    Se deja listo la nueva sección

---
+++ src/components/projects/projects.tsx
@@ -0,0 +1,12 @@
+
+function projects(){
+    return (
+        <section id="projects">
+            <div className="projects-main">
+                <h1>Projects</h1>
+            </div>
+        </section>
+    );
+}
+
+export default projects;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import Header from './components/header/header'
 import Home from './components/home/home'
 import About from './components/about/about'
+import Projects from './components/projects/projects'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
       <Header />
       <Home />
       <About />
+      <Projects />
     </body>
   </StrictMode>,
 )
