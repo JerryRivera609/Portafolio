869c2a33acfd5b5ce01dea81f65a4b0ad8b5b283
JerryRivera609 <jerry609rivera@gmail.com>
2025-01-16 22:42:50 -0500

    Se agregaron de manera dinamica, tarjetas con proyectos personales y seleccion permanente de cada sección

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,18 +1,62 @@
-import './projects.css'
+import React, { useState } from 'react';
+import './projects.css';
 
 const headerContent = [
-    {image: '../../../../public/llave-blanco.png', tittle:'Projects', url:'#'},
-    {image: '../../../../public/insignia.png', tittle:'Certificates', url:'#'},
-    {image: '../../../../public/skills.png', tittle: 'Tech Stack', url:'#'}
+    { image: '../../../../public/llave-blanco.png', tittle: 'Projects', url: '#' },
+    { image: '../../../../public/insignia.png', tittle: 'Certificates', url: '#' },
+    { image: '../../../../public/skills.png', tittle: 'Tech Stack', url: '#' }
 ];
 
+const targetData = [
+    {
+        tittle: 'Digimedia',
+        description: 'Desarrollo de sitio web en mis pracitcas, realizando una migración de PHP nativo a usar React, NodeJS, y MySQL.',
+        projectUrl: '../../../public/project1.png',
+        button: 'Details',
+        buttonImg: '../../../public/flecha.png',
+    },
+    {
+        tittle: 'Llama Tours',
+        description: 'Desarrollo de sitio web en personal, pagina de viajes en el Perú, usando HTML, CSS y JavaScript.',
+        projectUrl: '../../../public/project2.png',
+        button: 'Details',
+        buttonImg: '../../../public/flecha.png',
+    },
+    {
+        tittle: 'CV',
+        description: 'Primer proyecto basico de replica de mi CV personal, se usó HTML y CSS.',
+        projectUrl: '../../../public/project3.png',
+        button: 'Details',
+        buttonImg: '../../../public/flecha.png',
+    },
+];
 
+function Projects() {
+    const [selected, setSelected] = useState('Projects');
 
+    const header = headerContent.map(headers => (
+        <a
+            key={headers.tittle}
+            href='#projects'
+            onClick={() => setSelected(headers.tittle)} 
+            className={selected === headers.tittle ? 'selected' : ''}
+        >
+            <img src={headers.image} alt="" />
+            <p>{headers.tittle}</p>
+        </a>
+    ));
 
-function projects(){
-    const header = headerContent.map ( headers =>
-        <a href={headers.url}><img src={headers.image} alt="" /><p>{headers.tittle}</p></a>
-    );
+    const targets = targetData.map(target => (
+        <div className='project-target' key={target.tittle}>
+            <img src={target.projectUrl} width="380px" alt="" />
+            <h3>{target.tittle}</h3>
+            <p>{target.description}</p>
+            <div className="project-target-button">
+                <a href="">{target.button} <img src={target.buttonImg} width="20px" alt="" /></a>
+            </div>
+        </div>
+    ));
+
     return (
         <section id="projects">
             <div className="projects-main">
@@ -26,20 +70,13 @@ function projects(){
                     </div>
                     <div className="projects-show">
                         <div className="div-responsive projects-projects">
-                            <div className='project-target'>
-                                <img src="../../../public/project1.png" width="380px" alt="" />
-                                <h3>Digimedia</h3>
-                                <p>Desarrollo de sitio web en mis pracitcas, realizando una migración de PHP nativo a usar React, NodeJS, y MySQL.</p>
-                                <div className="project-target-button">
-                                    <a href="">Details <img src="../../../public/flecha.png" width="20px" alt="" /></a>
-                                </div>
-                            </div>
+                            {targets}
                         </div>
                         <div className="projects-certificate">
-
+                            {/* Agregar contenido según sea necesario */}
                         </div>
                         <div className="projects-techStack">
-
+                            {/* Agregar contenido según sea necesario */}
                         </div>
                     </div>
                 </div>
@@ -48,4 +85,4 @@ function projects(){
     );
 }
 
-export default projects;
+export default Projects;
