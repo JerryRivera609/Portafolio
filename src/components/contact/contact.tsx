7a417a82dd52c4aeeb8f5d05efada97fc374d6ac
JerryRivera609 <jerry609rivera@gmail.com>
2025-01-21 22:05:30 -0500

    Se completó la estructura del contact me

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -8,6 +8,28 @@ function contact(){
                     <h2>Contact Me</h2>
                     <p>Got a question? Send me a message, and I'll get back to you soon</p>
                 </div>
+                <div className="contact-email-redes">
+                    <div className="contact-email">
+                        <h3>Get in Touch</h3>
+                        <p>Have something to discuss? Send me a message and let's talk.</p>
+                        <form action="" method="get">
+                            <div>
+                                <input type="text" name="" id="" placeholder="Your Name" />
+                            </div>
+                            <div>
+                                <input type="email" name="" id="" placeholder="Your Email" />
+                            </div>
+                            <div>
+                                <input type="textarea" />
+                            </div>
+                        </form>
+                    </div>
+                    <div className="contact-redes">
+                        <div className="contact-redes-display">
+                            
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </section>
