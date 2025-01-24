02e5ec99780a03a10d69b7fd9d7da8681f2f2a6a
JerryRivera609 <jerry609rivera@gmail.com>
2025-01-24 11:04:54 -0500

    Se quitaron margin y padding globales que afectaban a los estilos

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,7 +3,7 @@ import './header.css'
 function header(){
     return(
         <>
-            <header className="p-5 w-full">
+            <header className="w-full">
                 <div className="p-5 flex justify-around items-center text-white relative z-[1000]">
                     <div className="nombre-Header">
                         <h2>Jerry Rivera</h2>
@@ -13,7 +13,7 @@ function header(){
                         <li>
                             <a href="#home" className="relative text-white no-underline group relative transition-all duration-1000 hover:text-[#870afc] hover:font-bold">
                                 Home
-                                <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-[#870afc] transition-all duration-300 ease-in-out group-hover:w-full"></span>
+                                <span className="absolute bottom-0 left-0 w-0 h-[2px]  bg-[#870afc] transition-all duration-300 ease-in-out group-hover:w-full"></span>
                             </a>
                         </li>
                         <li>
