c4c82022ce2537b1f4b2e53418efb35629e5ded3
JerryRivera609 <jerry609rivera@gmail.com>
2025-01-24 10:57:36 -0500

    Se reemplazó todo el css por tailwind en el header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,18 +3,39 @@ import './header.css'
 function header(){
     return(
         <>
-            <header>
-                <div className="main-Header">
+            <header className="p-5 w-full">
+                <div className="p-5 flex justify-around items-center text-white relative z-[1000]">
                     <div className="nombre-Header">
                         <h2>Jerry Rivera</h2>
                     </div>
                     <div className="links-Header">
-                        <ul>
-                            <li><a href="#home">Home</a></li>
-                            <li><a href="#about-me">About</a></li>
-                            <li><a href="#projects">Portfolio</a></li>
-                            <li><a href="#cotact-me">Contact</a></li>
-                        </ul>
+                    <ul className="flex gap-5 list-none">
+                        <li>
+                            <a href="#home" className="relative text-white no-underline group relative transition-all duration-1000 hover:text-[#870afc] hover:font-bold">
+                                Home
+                                <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-[#870afc] transition-all duration-300 ease-in-out group-hover:w-full"></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#about-me" className="relative text-white no-underline group relative transition-all duration-1000 hover:text-[#870afc] hover:font-bold">
+                                About
+                                <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-[#870afc] transition-all duration-300 ease-in-out group-hover:w-full"></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#projects" className="relative text-white no-underline group relative transition-all duration-1000 hover:text-[#870afc] hover:font-bold">
+                                Portfolio
+                                <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-[#870afc] transition-all duration-300 ease-in-out group-hover:w-full"></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#cotact-me" className="relative text-white no-underline group relative transition-all duration-1000 hover:text-[#870afc] hover:font-bold">
+                                Contact
+                                <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-[#870afc] transition-all duration-300 ease-in-out group-hover:w-full"></span>
+                            </a>
+                        </li>
+                    </ul>
+
                     </div>
                 </div>
             </header>
