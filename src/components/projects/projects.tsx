28bbb7018a7cef3b45cf8b97fedcd2c64a910de6
JerryRivera609 <jerry609rivera@gmail.com>
2025-02-17 21:51:09 -0500

    commit

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -24,13 +24,7 @@ const targetData = [
         buttonImg: '/flecha.png',
         url: 'https://jerryrivera609.github.io/LlamaTours/html/index.html'
     },
-    {
-        tittle: 'Curriculum Vitae',
-        description: 'Primer proyecto basico de replica de mi CV personal en una pagina web, se usó HTML y CSS.',
-        projectUrl: '/project3.png',
-        button: 'Live Demo',
-        buttonImg: '/flecha.png',
-    },
+    
 ];
 
 const skillTech = [
