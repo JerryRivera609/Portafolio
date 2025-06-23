606ef4dd39230f20b20d41d839c337b55e702b68
JerryRivera609 <jerry609rivera@gmail.com>
2025-06-23 15:38:25 -0500

    Mejoras y corrección de erorres de SEO

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -50,8 +50,8 @@ const targetData = [
         url: 'https://valor-actual-neto.vercel.app/'
     },
     {
-        tittle: 'Curriculum Vitae',
-        description: 'Primer proyecto basico de replica de mi CV personal en una pagina web, se usó HTML y CSS.',
+        tittle: 'Portafolio Personal',
+        description: 'Proyecto que contiene información personal, mis habilidades en la pogramación con información puntual de cada tema.',
         projectUrl: '/project3.png',
         button: 'Live Demo',
         buttonImg: '/flecha.png',
@@ -117,9 +117,9 @@ function Projects() {
             <h3 className='text-lg p-1' >{target.tittle}</h3>
             <p className='text-[0.8rem]'>{target.description}</p>
             <div className="w-full flex justify-end project-target-button">
-                <a href={target.url} target='_blank' aria-label='Live Demo' className='flex gap-2 bg-[#0000001e] p-2 w-auto h-auto items-center justify-center rounded-[10px] no-underline text-white text-[0.9rem] mt-2 border border-[#ffffff2b] transition-all duration-1000 hover:bg-[#e7e7e749]'>
+                <a href="#projects" target='_blank' aria-label='Live Demo' className='flex gap-2 bg-[#0000001e] p-2 w-auto h-auto items-center justify-center rounded-[10px] no-underline text-white text-[0.9rem] mt-2 border border-[#ffffff2b] transition-all duration-1000 hover:bg-[#e7e7e749]'>
                     {target.button}
-                    <img src={target.buttonImg} className='w-4 h-auto' alt="" />
+                    <img src={target.buttonImg} className='w-4 h-auto' alt="Flecha-Izquierda" />
                 </a>
             </div>
         </div>
